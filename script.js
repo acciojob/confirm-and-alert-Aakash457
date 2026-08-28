eaf84f9d93b611da619b799d0bb112a5ed0d33c8
Aakash457 <114485477+Aakash457@users.noreply.github.com>
2026-08-28 21:04:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const proceed = confirm("Do you want to proceed?")
+
+if(proceed){
+	console.log("You clicked OK. Proceeding...")
+}else{
+	console.log("You clicked Cancel. Exiting...")
+}
